9a62ec38b82ec4ff5e7d5963a5ab708ad9742cb8
Joaquin-Sanchez-Cerdan <jac.sanchez@aumno.etec.um.edu.ar>
2024-07-02 10:19:41 -0300

    Login page

--- douggies/src/pages/login/login.jsx
+++ douggies/src/pages/login/login.jsx
@@ -2,7 +2,27 @@ import "./login.scss";
 
 const Login = () => {
     return (
-        <div className="login">Login</div>
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello World</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Perferendis quia enim animi doloremque blanditiis a dignissimos consequatur aliquam 
+                        commodi tenetur accusantium, in molestiae veritatis aperiam reprehenderit laborum iusto, 
+                        fugiat nesciunt. </p>
+                    <span>Don't you have an account?</span>
+                    <button>Register</button>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input type="text" placeholder="Username"/>
+                        <input type="password" placeholder="Password"/> 
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 };
 
