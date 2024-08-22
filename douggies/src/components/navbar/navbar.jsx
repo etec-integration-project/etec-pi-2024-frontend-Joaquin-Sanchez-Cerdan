a53bc6f6955b2ead3767ce7a58eba735d7bde442
Joaquin-Sanchez-Cerdan <jac.sanchez@alumno.etec.um.edu.ar>
2024-08-22 09:35:45 -0300

    navbar

--- douggies/src/components/navbar/navbar.jsx
+++ douggies/src/components/navbar/navbar.jsx
@@ -1,8 +1,39 @@
-import navbar from './navbar.scss';
+import './navbar.scss';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className='navbar'>NavBar</div>
+    <div className='navbar'>
+      <div className="left">
+        <Link to="/" style={{textDecoration:"none"}}>
+          <span>Douggies</span>
+          <HomeOutlinedIcon/>
+          <DarkModeOutlinedIcon/>
+          <GridViewOutlinedIcon/>
+          <div className="search">
+            <SearchOutlinedIcon/>
+            <input type="text" placeholder='Search...'/>
+          </div>
+        </Link>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon/>
+        <EmailOutlinedIcon/>
+        <NotificationsOutlinedIcon/>
+        <div className="user">
+          <img src="" alt="" />
+          <span>Ju Mozz</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
