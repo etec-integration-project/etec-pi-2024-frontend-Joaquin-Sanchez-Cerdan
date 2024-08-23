ff2b07e622bc72633c9b2244b05ad82303a27716
Joaquín Sanchez <90327045+Joaquin-Sanchez-Cerdan@users.noreply.github.com>
2024-08-23 10:23:02 -0300

    Add files via upload

---
+++ douggies/src/App.js
@@ -0,0 +1,78 @@
+import Login from "./pages/login/login";
+import Register from "./pages/register/register";
+import NavBar from "./components/navbar/navbar";
+import LeftBar from "./components/leftBar/leftBar";
+import RightBar from "./components/rightBar/rightBar";
+import Home from "./pages/home/home";
+import Profile from "./pages/profile/profile";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+function App() {
+
+  const currentUser = true;
+
+  const Layout= ()=>{
+    return(
+      <div>
+        <NavBar/>
+        <div style={{display: "flex"}}>
+          <LeftBar/>
+          <div style={{ flex: 6}}>
+            <Outlet/>
+          </div>
+          <RightBar/>
+        </div>
+      </div>
+    )
+  }
+
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser){
+      return <Navigate to="/login"/>;
+    }
+
+    return children;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+  return (
+    <div>
+       <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
